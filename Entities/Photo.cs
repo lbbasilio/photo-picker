4fce3667558e2da8711389600fdc6d201202ab1b
lbbasilio <lbasilio@tutanota.com>
2023-03-28 01:30:36 -0700

    Created Photo entity and added Id, HashedPassword, LastLogin and Role fields to User

---
+++ Entities/Photo.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#pragma warning disable CS8618 // Suppress "Non-nullable field must contain a non-null value when exiting constructor." warning
+namespace PhotoPicker.Entities
+{
+    public class Photo
+    {
+        public long Id { get; set; }
+        public DateTime UploadDate { get; set; }
+        public PhotoState State { get; set; }
+        public MediaType MediaType { get; set; }
+        public DateTime? ExtractedDate { get; set; }
+        public string S3Url { get; set; }
+        public long UploaderId { get; set; }
+        public long? RecognizedUserId { get; set; }
+        public int? AlogrithmConfidence { get; set; }
+        public long? SelectedUserId { get; set; }
+
+        #region Navigation Properties
+        [ForeignKey("UploaderId")]
+        public virtual User? Uploader { get; set; }
+        [ForeignKey("RecognizedUserId")]
+        public virtual User? RecognizedUser { get; set; }
+        [ForeignKey("SelectedUserId")]
+        public virtual User? SelectedUser { get; set; }
+        #endregion
+    }
+
+    public enum PhotoState
+    {
+        ProcessingPending,
+        NoFacesFound,
+        NoMatchesFound,
+        MatchedOne,
+        MatchedMultiple,
+        UploaderConfirmed
+    }
+
+    public enum MediaType
+    {
+        Photo,
+        Video
+    }
+}
+#pragma warning restore CS8618
--- Entities/User.cs
+++ Entities/User.cs
@@ -1,9 +1,20 @@
-﻿namespace PhotoPicker.Entities
+﻿#pragma warning disable CS8618 // Suppress "Non-nullable field must contain a non-null value when exiting constructor." warning
+namespace PhotoPicker.Entities
 {
     public class User
     {
+        public long Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string Hash { get; set; }
+        public string HashedPassword { get; set; }
+        public DateTime LastLogin { get; set; }
+        public UserRole Role { get; set; }
+    }
+
+    public enum UserRole
+    {
+        User,
+        Photographer
     }
 }
+#pragma warning restore CS8618
